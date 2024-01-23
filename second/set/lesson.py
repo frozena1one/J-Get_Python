b66e6f5eb2f722fa8c47d8fbcc1b75e71a7422ea
Ryazapov Kamil <ryazapov.kamil@inbox.ru>
2024-01-23 20:19:38 +0300

    edit lesson.py

--- second/set/lesson.py
+++ second/set/lesson.py
@@ -16,12 +16,14 @@ print(len(a))
 # Объединение (| or union):
 a = {1, 2, 3, 4, 5}
 b = {3, 4, 5, 6, 7}
+c = a | b # -> c: 1, 2, 3, 4, 5, 6, 7
 print(a | b)
 print(a.union(b))
 
 # Пересечение (& or intersection):
 a = {1, 2, 3, 4, 5}
 b = {3, 4, 5, 6, 7}
+c = a & b # -> c: 3, 4, 5
 print(a & b)
 print(a.intersection(b))
 
